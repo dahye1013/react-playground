d392d83d2ff4c9247697d008abde2aa6331ce9dd
dahye1013 <alrean@naver.com>
2022-06-29 14:35:01 +0900

    feat: render conditional content

--- 01-basic/src/components/Expenses/ExpenseItem.jsx
+++ 01-basic/src/components/Expenses/ExpenseItem.jsx
@@ -4,15 +4,17 @@ import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 // [StateLess Component] - not have any internal state
-const ExpenseItem = (expense) => {
+const ExpenseItem = ({ expense }) => {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={expense.date} />
-      <div className="expense-item__description">
-        <h2>{expense.title}</h2>
-        <div className="expense-item__price">{expense.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={expense.date} />
+        <div className="expense-item__description">
+          <h2>{expense.title}</h2>
+          <div className="expense-item__price">{expense.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
--- 01-basic/src/components/Expenses/Expenses.jsx
+++ 01-basic/src/components/Expenses/Expenses.jsx
@@ -1,14 +1,24 @@
-import ExpenseItem from './ExpenseItem';
+import React, { useState } from 'react';
 import './Expenses.css';
-
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = ({ expenses }) => {
+  const [filteredYear, setFilteredYear] = useState('2022');
+  const filteredExpenses = expenses.filter(
+    (expense) => expense.date.getFullYear() === Number(filteredYear),
+  );
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <div className="expenses">
-      {expenses.map((expense, index) => (
-        <ExpenseItem key={index} expense={expense}></ExpenseItem>
-      ))}
-    </div>
+    <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onFilterExpense={filterChangeHandler} />
+      <ExpensesList items={filteredExpenses} />
+    </Card>
   );
 };
 
