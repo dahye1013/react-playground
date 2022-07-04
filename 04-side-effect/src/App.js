82be16241b2a317845a25c94e1a94fd0ff79030a
dahye1013 <alrean@naver.com>
2022-07-04 23:23:02 +0900

    feat: create Auth context

--- 04-side-effect/src/App.js
+++ 04-side-effect/src/App.js
@@ -4,32 +4,36 @@ import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
+import AuthContext from './store/auth-context';
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn', true);
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
   useEffect(() => {
     const storedLoginInfo = localStorage.getItem('isLoggedIn');
-    if (storedLoginInfo === true) {
-      setIsLoggedIn(false);
+    if (storedLoginInfo) {
+      setIsLoggedIn(true);
     }
   }, []);
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider value={{ isLoggedIn }}>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
